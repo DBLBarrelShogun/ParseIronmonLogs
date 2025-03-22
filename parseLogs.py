afaaac30a1a48d9c4c312885ddd48bd5388ccd71
DBLBarrelShogun <148076086+DBLBarrelShogun@users.noreply.github.com>
2025-03-22 00:59:06 +0000

    Add files via upload
    Changes to readability

--- parseLogs.py
+++ parseLogs.py
@@ -16,31 +16,32 @@ for logfile in os.listdir("logs"):
             pline = line.rstrip()
             if pline.startswith("Random Seed:"):
                 seedString = pline.replace("Random Seed: ","")
-            
-            if pline.startswith("--Randomized Evolutions--"):
-                startParse = "Y"
-                endParse = "N"
-                continue
-            if pline.startswith("--Pokemon Base Stats & Types--"):
-                startParse = "N"
-                endParse = "Y"
-                break
-            if startParse == "Y" and pline != "":
-                pokemon = pline.split(" and ",1)
-                pokemon = pokemon[0].replace(" ","").split("->")
-                pokeevo = ",".join(pokemon)
-                print(seedString.rstrip() + "," + pokeevo.rstrip())
             '''
             if pline.startswith("--Pokemon Movesets--"):
                 startParse = "Y"
                 endParse = "N"
                 continue
-            if pline.startswith("--TM Moves--"):
-                startParse = "N"
-                endParse = "Y"
-                break
             if startParse == "Y" and "->" in pline:
                 pokemon = pline.replace(" ","").split("->")
                 pokeevo = ",".join(pokemon)
                 print(seedString.rstrip() + "," + pokeevo.rstrip())
+            if pline.startswith("--TM Moves--"):
+                startParse = "N"
+                endParse = "Y"
+                break
             '''
+            if pline.startswith("--Randomized Evolutions--"):
+                startParse = "Y"
+                endParse = "N"
+                continue
+            if startParse == "Y" and pline != "":
+                pokemon = pline.split(" and ",1)
+                pokemon = pokemon[0].replace(" ","").split("->")
+                pokeevo = ",".join(pokemon)
+                print(seedString.rstrip() + "," + pokeevo.rstrip())
+            
+            if pline.startswith("--Pokemon Base Stats & Types--"):
+                startParse = "N"
+                endParse = "Y"
+                break
+            
